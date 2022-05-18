2b046f547857ddc9b7d25d4bb5efde04dd16e4da
unknown <zsda3131@naver.com>
2022-05-18 13:53:20 +0900

    20번까지만 나옴

--- movie/movie.py
+++ movie/movie.py
@@ -7,80 +7,48 @@ import pandas as pd
 from pprint import pprint
 # import os
 # import getpass
-
 # username = getpass.getuser() #컴퓨터 사용자 이름 가져오기
 # from pathlib import Path  
 # filepath = Path('folder/subfolder/out.csv')  
 # filepath.parent.mkdir(parents=True, exist_ok=True)  
 
 
-# f = open('C:/Users/82102/Desktop/movie/movie_data_a.csv', encoding='cp949')
-data = pd.read_csv("out.csv", usecols=[3])
-selectdata = pd.DataFrame(data, columns=['id'])
-print(selectdata)
-# # print(type(data))
-df_dict = data.to_dict()
-dic_val = df_dict['id'].values()
-dic_list = list(dic_val)
-print(dic_list)
+
 TMDB_API_KEY = 'e6149bf41aaefec295f3595be639b5c7'
 
-# def get_movie_datas():
-total_data = {}
 
-    # 1페이지부터 500페이지까지 (페이지당 20개, 총 10,000개)
 def make_csv():
+    # 1페이지부터 500페이지까지 (페이지당 20개, 총 10,000개)
     for i in range(1, 10):
         request_url = f"https://api.themoviedb.org/3/movie/popular?api_key={TMDB_API_KEY}&language=ko-KR&page={i}"
         movies = requests.get(request_url).json()
         # print(movies)
         datafield = pd.DataFrame(movies['results'])
-    # print(datafield)
+        # print(datafield)
         datafield_preprocessed = datafield[
             ['backdrop_path', 'genre_ids', 'id', 'overview', 'popularity', 'release_date', 'vote_average', 'poster_path', 'title']]
     # print(datafield_preprocessed)
         result = datafield_preprocessed
         result = pd.concat([result, datafield_preprocessed], ignore_index=True)
-    result.to_csv('out.csv', encoding="utf-8-sig") 
+    result.to_csv('out1.csv', encoding="utf-8-sig") 
+
+# make_csv()
 
 
 
-    # print(result)
 
-# get_movie_datas()
 
-#         for movie in movies['results']:
-#             if movie.get('release_date', ''):
-#                 fields = {
-#                     'movie_id': movie['id'],
-#                     'title': movie['title'],
-#                     'released_date': movie['release_date'],
-#                     'popularity': movie['popularity'],
-#                     'vote_avg': movie['vote_average'],
-#                     'overview': movie['overview'],
-#                     'poster_path': movie['poster_path'],
-#                     # 'genres': movie['genre_ids']
-#                 }
+# ott 정보받기
+data = pd.read_csv("out.csv", usecols=[3])
+selectdata = pd.DataFrame(data, columns=['id'])
+print(selectdata)
+df_dict = data.to_dict()
+dic_val = df_dict['id'].values()
+dic_list = list(dic_val)
+# print(dic_list)
 
-#                 data = {
-#                     "pk": movie['id'],
-#                     "model": "movies.movie",
-#                     "fields": fields
-#                 }
-
-#                 total_data.append(data)
-
-#     with open("movie_data.json", "w", encoding="utf-8") as w:
-#         json.dump(total_data, w, ensure_ascii=False)
-
-# get_movie_datas()
-
-# TMDB_API_KEY = ''
-# print(dic_list[0:10])
-# def get_movie_datas():
-#     total_data = []
-
-#     # 1페이지부터 500페이지까지 (페이지당 20개, 총 10,000개)
+'''
+total_data = {}
 for i in dic_list:
 
     request_url = f"https://api.themoviedb.org/3/movie/{i}/watch/providers?api_key={TMDB_API_KEY}"
@@ -100,29 +68,17 @@ for i in dic_list:
                 total_data[i] = flatrate['provider_name']
 print(total_data)
 
-#         movie = movies['results']
-#         print(movie)
-#         kr_movie = movie['KR']
-#         print(kr_movie)
-#         ott_movie = kr_movie['flatrate']
-#         print(ott_movie)
-#         for ott in ott_movie: 
-#             fields = {
-#                 'provider_id': ott['provider_id'],
-#                 'logo_path': ott['logo_path'],
-#                 'provider_name': ott['provider_name']
+'''
+total_data = {}
+for i in dic_list:
 
-#             }
+    request_url = f"https://api.themoviedb.org/3/movie/{i}?api_key={TMDB_API_KEY}&language=en-US"
+    movies = requests.get(request_url).json()
+    if movies.get('runtime'):
+        total_data[i] = movies['runtime']
+print(total_data)
 
-#             data = {
-#                 "pk": i,
-#                 "model": "movies.movie",
-#                 "fields": fields
-#             }
 
-#             total_data.append(data)
 
-#     with open("ott_data.json", "w", encoding="utf-8") as w:
-#         json.dump(total_data, w, ensure_ascii=False)
 
-# get_movie_datas()
+
