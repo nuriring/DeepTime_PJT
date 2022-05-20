2029332c09a5d63469b7b94986f20fd96fda28b0
hde5418 <hde5418@naver.com>
2022-05-20 14:13:34 +0900

    model

--- articles/models.py
+++ articles/models.py
@@ -1,18 +1,22 @@
-from tkinter import CASCADE
 from django.db import models
 from django.conf import settings
 
+from movies.models import Movie
+
+
 # Create your models here.
 
-class Article(models.model):
+class Article(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='articles')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='detail_review')
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    category = models.CharField(max_length=200)
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_articles')
 
-class Comment(models.model):
+class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
     content = models.CharField(max_length=200)
--- movies/models.py
+++ movies/models.py
@@ -1,11 +1,23 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+class Genre(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+
 class Movie(models.Model):
-    title = models.CharField(max_length=100)
-    release_date = models.DateField()
-    popularity = models.FloatField()
-    vote_count = models.IntegerField()
-    vote_average = models.FloatField()
+    id = models.IntegerField(primary_key=True)
+    backdrop_path = models.TextField()
+    genre = models.ManyToManyField(Genre, related_name='movie_genre')
     overview = models.TextField()
-    poster_path = models.CharField(max_length=200)
+    popularity = models.FloatField()
+    release_date = models.DateField()
+    vote_average = models.FloatField()
+    poster_path = models.TextField()
+    title = models.CharField(max_length=50)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_movies')
+
+class OTT(models.Model):
+    id = models.IntegerField(primary_key=True)
+    movie_id = models.IntegerField()
+    provider_name = models.CharField(max_length=50)
