707adfc52f40c61f14cef1de9fa32dc6aa2c00fb
hde5418 <hde5418@naver.com>
2022-05-24 12:42:13 +0900

    back 완료

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from articles.models import Article
 from articles.serializers.comment import CommentSerializer
-from movies.models import Genre
+from movies.models import Genre, Review
 from dj_rest_auth.registration.serializers import RegisterSerializer
 User = get_user_model()
 
@@ -28,13 +28,12 @@ class ProfileSerializer(serializers.ModelSerializer):
             model = Article
             fields = ('id', 'title', 'content') #게시글 에서 보여줄 필드 : id, 제목, 내용 
 
-    #2
-    # class GenreSerializer(serializers.ModelSerializer): #프로필에서 유저가 좋아하는 장르 보여주기
-    #     class Meta:
-    #         model = Genre
-    #         fields = ('id', 'name')
-
-    #1
+    class ReviewSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Review
+            fields = ('id','user','movie_id','title','content','created_at') 
+    
+    reviews = ReviewSerializer(many=True)
     like_articles = ArticleSerializer(many=True) #유저가 좋아요한 게시글
     articles = ArticleSerializer(many=True) #유저가 작성한 게시글
     comments = CommentSerializer(many=True, read_only=True) #유저가 작성한 댓글
@@ -45,7 +44,7 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
 
-        fields = ('id', 'username', 'like_articles', 'articles', 'genre', 'comments') #유저가 좋아하는 장르도 보여주고 싶은데 이렇게 될라나 모르겠넹
+        fields = ('id', 'username', 'like_articles', 'articles', 'genre', 'comments', 'reviews') #유저가 좋아하는 장르도 보여주고 싶은데 이렇게 될라나 모르겠넹
 
 
     
